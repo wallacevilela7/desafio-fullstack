dda7d683d934d6fe6dd6f3900c25f4c490c0c989
wallacevilela7 <wallace.vilela9@gmail.com>
2025-05-26 11:21:35 -0300

    Create spring app

---
+++ backend/app-desafio/src/main/java/tech/wvs/appdesafio/AppDesafioApplication.java
@@ -0,0 +1,13 @@
+package tech.wvs.appdesafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppDesafioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppDesafioApplication.class, args);
+    }
+
+}
---
+++ backend/app-desafio/src/test/java/tech/wvs/appdesafio/AppDesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.wvs.appdesafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppDesafioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
